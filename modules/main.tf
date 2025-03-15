253c0fc9f9156662cabed7a277aa364460cec1d7
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-15 22:58:18 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,8 +1,12 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
+}
+
+output "instance_id" {
+  value = aws_instance.example.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
 }
