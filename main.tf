bfeab52b4dc7e3a958d195ca6bf0d15557045d70
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 19:53:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-      access_key = var.aws_access_key
-      secret_key = var.aws_secret_key
     }
   }
 }
