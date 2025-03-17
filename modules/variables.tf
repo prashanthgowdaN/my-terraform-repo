a68f62c303f9adac1e78c62b9101dc6111a3338d
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 15:55:26 +0530

    Delete modules/variables.tf

--- modules/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "vm_names" {
-  type    = list(string)
-  default = ["web-server", "db-server"]  # We can use the Host Name and its only for single Image 
-}
