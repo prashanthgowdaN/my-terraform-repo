1fde7aa6eae290ced84242e8092e26bab7be442a
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 21:29:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ required_providers {
 
 module "EC2_VM" {
   source        = "./modules/dev"  # Path to the module directory
-  aws_region    = "us-east-1"
-  instance_type = "t2.micro"
-  ami_id        = "ami-08b5b3a93ed654d19"  # Replace with your AMI ID
-  instance_name = "my-terraform-vm"
+  #aws_region    = "us-east-1"
+  #instance_type = "t2.micro"
+  #ami_id        = "ami-08b5b3a93ed654d19"  # Replace with your AMI ID
+  #instance_name = "my-terraform-vm"
 }
 
