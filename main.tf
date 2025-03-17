dadd6d19d4c21b5f8279a2df9392fcb857d1d406
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 21:15:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ required_providers {
     }
   }
 }
+
+# main.tf (root directory)
+
+module "vm_module" {
+  source        = "./modules/dev"  # Path to the module directory
+  aws_region    = "us-east-1"
+  instance_type = "t2.micro"
+  ami_id        = "ami-08b5b3a93ed654d19"  # Replace with your AMI ID
+  instance_name = "my-terraform-vm"
+}
+
