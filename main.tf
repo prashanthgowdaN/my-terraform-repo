a6efe0b106d3ed9fba0ee45ac8550e194a7acc26
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 22:57:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 # main.tf (root directory)
 
 module "EC2_VM" {
-  source        = "./modules/EC2_instance"  # Path to the module directory
+  source        = "./modules/EC2_Instance"  # Path to the module directory
   #aws_region    = "us-east-1"
   #instance_type = "t2.micro"
   #ami_id        = "ami-08b5b3a93ed654d19"  # Replace with your AMI ID
