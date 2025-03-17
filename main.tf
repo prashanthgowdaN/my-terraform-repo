1566b64e462d974c34bdf87fbb56494d53d70dd7
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 23:05:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   access_key       = var.aws_access_key
   secret_key       = var.aws_secret_key
-  aws_region           = var.aws_region
+  region           = var.region
 }
 # main.tf (root directory)
 
 module "EC2_VM" {
   source        = "./modules/EC2_Instance"  # Path to the module directory
-  aws_region    = "us-east-1"
+  region    = "us-east-1"
   instance_type = "t2.micro"
   ami_id        = "ami-08b5b3a93ed654d19"  # Replace with your AMI ID
   #instance_name = "my-terraform-vm"
