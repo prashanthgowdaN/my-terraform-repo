58453dc45c99ff6722d30a8c36cfb6e183382752
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 17:28:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
   region = "us-east-1"  # Choose your region
   access_key = var.aws_access_key
-  secret_key = var.aws_secret_key 
+  secret_key = var.aws_secret_key
+}
 module "prashanth_test_VM" {
   source = ".//modules/dev"
 }
