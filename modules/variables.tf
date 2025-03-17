a2904f0f64d1de62898eb600d8b78b1e594f3e49
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 15:47:44 +0530

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,2 +1,6 @@
-aws_access_key = "AKIAVFIWISPQPCZOFGSO"
-aws_secret_key = "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"
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "vm_names" {
+  type    = list(string)
+  default = ["web-server", "db-server"]  # We can use the Host Name and its only for single Image 
+}
