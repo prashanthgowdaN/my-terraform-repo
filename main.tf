77195931529abcbfe8a835266f2fb42ad3195d0b
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 16:24:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  region = "us-east-1"  # Choose your region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key 
+module "prashanth_test_VM" {
+  source = ".//modules/dev"
+}
