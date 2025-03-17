b3216f08c75dfff3ac690d9ccdf75ba01e89ad6b
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 15:19:57 +0530

    Update main.tf

--- modules/EC2_Instance/main.tf
+++ modules/EC2_Instance/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 resource "aws_instance" "testVM" {
   ami           = var.ami_id
   instance_type = var.instance_type
