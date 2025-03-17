a8e0a703781911c526cce0c26428d5407cd1ab32
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 19:50:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
-provider "aws" {
-  region = "us-east-1"  # Choose your region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-module "prashanth_test_VM" {
-  source = "github.com/prashanthgowdaN/my-terraform-repo//modules/dev"
+terraform {
+  required_version = ">= 1.0.0"
+required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+      region = "us-east-1"  # Choose your region
+      access_key = var.aws_access_key
+      secret_key = var.aws_secret_key
+    }
+  }
 }
