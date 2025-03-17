dd55305ae85edf1e5c9c4157ee2ce054acee36ed
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 22:46:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,2 +1,11 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
