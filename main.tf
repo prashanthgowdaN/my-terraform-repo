1be3d6958c9f9761fc30ac21916b790d7943d41f
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-17 19:53:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-      region = "us-east-1"  # Choose your region
       access_key = var.aws_access_key
       secret_key = var.aws_secret_key
     }
