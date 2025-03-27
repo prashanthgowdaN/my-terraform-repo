a2385b554d22472c3a03edfd30290f9e1397c550
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-27 15:52:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # main.tf (root directory)
 
 module "EC2_VM01" {
-  source        =  "./modules/EC2_Instance"  # Path to the module directory
+  source        =  "./modules/dev"  # Path to the module directory
   instance_type =  "var.aws_instance_type" 
   ami_id        =  "var.aws_ami"  # Replace with yourI ID
   #instance_name = "my-terraform-vm"
