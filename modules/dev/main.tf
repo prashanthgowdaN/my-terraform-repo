1bdd0adaff8c593a7f669143df829dd9a2263811
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-28 22:46:45 +0530

    Update main.tf

--- modules/dev/main.tf
+++ modules/dev/main.tf
@@ -7,7 +7,7 @@
 
 resource "aws_instance" "vm" {
   ami_id        = var.ami_id
-  #instance_type = var.instance_type
+  instance_type = var.instance_type
   #key_name      = var.key_name
   #subnet_id     = data.aws_subnet_ids.default_subnet.ids[0]  # Use the default subnet ID
   #vpc_security_group_ids = [data.aws_security_group.default_sg.id]  # Use the default security group
