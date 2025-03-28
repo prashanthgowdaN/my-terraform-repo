bc2cced8d31f0e469d8c73bc4dc7a131cd0cc76a
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-28 23:00:32 +0530

    Update variables.tf

--- modules/dev/variables.tf
+++ modules/dev/variables.tf
@@ -22,7 +22,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "The type of instance to create"
   type        = string
-  default     = "t2.micro"  # Default instance type
+  #default     = "t2.micro"  # Default instance type
 }
 
 /*variable "key_name" {
