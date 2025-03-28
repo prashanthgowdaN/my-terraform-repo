85c5306a6ee57c7df48f7092365e5b5bee284e7c
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-29 00:12:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 module "EC2_VM01" {
   source        =  "./modules/dev"  # Path to the module directory
   instance_type =  "var.aws_instance_type" 
-  ami_id        =  "var.aws_ami"  # Replace with yourI ID
+  ami_id        =  "ami-08b5b3a93ed654d19"  # Replace with yourI ID
   aws_access_key       = var.aws_access_key
   aws_secret_key       = var.aws_secret_key
   #instance_name = "my-terraform-vm"
