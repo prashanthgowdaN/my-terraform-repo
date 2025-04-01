962a0e18443d4a70e132a364fef8507b713adfc2
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-01 23:32:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  version = "~> 4.0"
+  version = "~> 5.0"
   #aws_access_key       = var.aws_access_key
   #aws_secret_key       = var.aws_secret_key
   region           = var.aws_region
