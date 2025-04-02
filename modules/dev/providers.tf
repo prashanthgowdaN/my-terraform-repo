02ba3b58469de605d5092a2a10c02f9283d489a0
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-02 19:23:12 +0530

    Update providers.tf

--- modules/dev/providers.tf
+++ modules/dev/providers.tf
@@ -6,10 +6,12 @@
 }*/
 
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~>5.0"
-    }
-  }
-}
+   required_version = ">= 1.0.0"
+ 
+   required_providers {
+     aws = {
+       source  = "hashicorp/aws"
+       version = "~> 5.0"
+     }
+   }
+ }
