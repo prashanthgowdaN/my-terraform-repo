77babc02ea63f7cbc75d514c144c31223e120c5c
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-02 20:48:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,13 @@ module "EC2_VM02" {
   #instance_name = "my-terraform-vm"
 }
 
+resource "aws_instance" "Template" {
+  ami = "ami-08b5b3a93ed654d19" 
+  instance_type  = "t2.micro"
+  #key_name      = var.key_name
+  #subnet_id     = data.aws_subnet_ids.default_subnet.ids[0]  # Use the default subnet ID
+  #vpc_security_group_ids = [data.aws_security_group.default_sg.id]  # Use the default security group
+
 /*module "EC2_VM01" {
   source        =  "./modules/EC2_Instance"  # Path to the module directory
   instance_type =  "var.aws_instance_type" 
