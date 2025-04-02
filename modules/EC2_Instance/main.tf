262b94728f06c6c7de6934e89e6a54e30345c06f
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-02 18:30:31 +0530

    Update main.tf

--- modules/EC2_Instance/main.tf
+++ modules/EC2_Instance/main.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+  #aws_region = var.aws_region
+  region = "us-east-1" 
+  aws_access_key = var.aws_access_key
+  aws_secret_key = var.aws_secret_key
+}
+
 resource "aws_instance" "prashanthVM" {
   ami = "ami-08b5b3a93ed654d19"
   instance_type = "t2.micro"
