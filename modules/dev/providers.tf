e8a7442b70dc665988c00f4a4073d901ff9ccba4
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-02 17:56:12 +0530

    Update providers.tf

--- modules/dev/providers.tf
+++ modules/dev/providers.tf
@@ -3,15 +3,13 @@
   #region = "us-east-1" 
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-}
+}*/
 
 terraform {
-  required_version = ">= 1.0.0"
-
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source = "hashicorp/aws"
+      version = "~>5.0"
     }
   }
-}*/
+}
