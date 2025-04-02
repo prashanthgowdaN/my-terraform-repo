3c2ae437e0335180bd4cbdb128c737e1ff1141a8
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-02 17:09:30 +0530

    Update main.tf

--- modules/EC2_Instance/main.tf
+++ modules/EC2_Instance/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 resource "aws_instance" "prashanthVM" {
   ami = "ami-08b5b3a93ed654d19"
   instance_type = "t2.micro"
