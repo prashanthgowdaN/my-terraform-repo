8590005402a8b50e49387bfbce1391538d795c24
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-02 21:51:29 +0530

    Update main.tf

--- modules/EC2_Instance/main.tf
+++ modules/EC2_Instance/main.tf
@@ -1,4 +1,20 @@
-terraform {
+module "EC2VMs" {
+  source        =  "./modules  # Path to the module directory
+  instance_type  = "t2.micro" 
+  ami_id        =  "ami-08b5b3a93ed654d19"  # Replace with yourI ID
+  #instance_name = "my-terraform-vm"
+}
+tags = {
+    Name = "PrashanthV"
+    Environment = "Prod"
+    TestENV     = "Destroy"
+  }
+}
+output "instance_id" {
+  value = aws_instance.TestVM.id
+}
+
+/*terraform {
    required_version = ">= 1.0.0"
  
    required_providers {
@@ -21,4 +37,4 @@ resource "aws_instance" "prashanthVM" {
 }
 output "instance_id" {
   value = aws_instance.prashanthVM.id
-}
+}*/
