f6b71d7442973eb3315c7647b40c25d2797fbe1d
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-02 18:39:38 +0530

    Update main.tf

--- modules/EC2_Instance/main.tf
+++ modules/EC2_Instance/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   #aws_region = var.aws_region
   region = "us-east-1" 
-  aws_access_key = var.aws_access_key
-  aws_secret_key = var.aws_secret_key
+  #aws_access_key = var.aws_access_key
+  #aws_secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "prashanthVM" {
