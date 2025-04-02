b7b52e366a461371caa3a79254041b3913a54d7c
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-02 17:15:10 +0530

    Update providers.tf

--- modules/EC2_Instance/providers.tf
+++ modules/EC2_Instance/providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  aws_region = var.aws_region
-  #region = "us-east-1" 
+  #aws_region = var.aws_region
+  region = "us-east-1" 
   aws_access_key = var.aws_access_key
   aws_secret_key = var.aws_secret_key
 }
