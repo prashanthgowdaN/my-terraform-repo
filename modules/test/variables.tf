c8e2f10f630ae7e31e87bd811e7c146aa55e5bb2
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-02 20:46:12 +0530

    Update variables.tf

---
+++ modules/test/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string 
+  sensitive   = true 
+}
+
+variable "aws_instance_type" {
+  description = "AWS Instance Type"
+  type        = string 
+  sensitive   = true 
+}
