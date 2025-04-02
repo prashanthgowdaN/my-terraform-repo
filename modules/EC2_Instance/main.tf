09a8a5b1fb5eafce9a5707071a19ca88be7cedbe
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-02 18:50:48 +0530

    Update main.tf

--- modules/EC2_Instance/main.tf
+++ modules/EC2_Instance/main.tf
@@ -1,9 +1,13 @@
-/*provider "aws" {
-  #aws_region = var.aws_region
-  region = "us-east-1" 
-  #aws_access_key = var.aws_access_key
-  #aws_secret_key = var.aws_secret_key
-}*/
+terraform {
+   required_version = ">= 1.0.0"
+ 
+   required_providers {
+     aws = {
+       source  = "hashicorp/aws"
+       version = "~> 5.0"
+     }
+   }
+ }
 
 resource "aws_instance" "prashanthVM" {
   ami = "ami-08b5b3a93ed654d19"
