624e8f5a9f5d3cb9040eb2563b5aa5d627f03c1c
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-02 17:44:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ terraform {
   }
 }
 
-provider "aws" {
+/*provider "aws" {
   #aws_access_key       = var.aws_access_key
   #aws_secret_key       = var.aws_secret_key
   region           = var.aws_region
-}
+}*/
 
 # main.tf (root directory)
 
