b97758d7659556e8e1b420caa91d70229418e904
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-03 19:16:04 +0530

    Update main.tf

--- modules/test/main.tf
+++ modules/test/main.tf
@@ -1,4 +1,42 @@
-module "TestingVM" {
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vm_1" {
+  source        = "../modules"
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  instance_name = "dev-vm-1"
+}
+
+module "vm_2" {
+  source        = ""../modules"
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  instance_name = "dev-vm-2"
+}
+
+module "vm_3" {
+  source        = ""../modules"
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  instance_name = "dev-vm-3"
+}
+
+output "vm_1_instance_id" {
+  value = module.vm_1.instance_id
+}
+
+output "vm_2_instance_id" {
+  value = module.vm_2.instance_id
+}
+
+output "vm_3_instance_id" {
+  value = module.vm_3.instance_id
+}
+
+
+/*module "TestingVM" {
   source        =  "./modules  # Path to the module directory
   instance_type  = "t2.micro" 
   ami_id        =  "ami-08b5b3a93ed654d19"  # Replace with yourI ID
@@ -14,7 +52,7 @@ output "instance_id" {
   value = aws_instance.TestVM.id
 }
 
-/*resource "aws_instance" "TestVM" {
+resource "aws_instance" "TestVM" {
   ami = "ami-08b5b3a93ed654d19" 
   instance_type  = "t2.micro"
   #key_name      = var.key_name
