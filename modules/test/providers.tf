86ee005420a69959fb40c204a0ac8731dd5d2f86
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-03 20:06:07 +0530

    Update providers.tf

--- modules/test/providers.tf
+++ modules/test/providers.tf
@@ -1,5 +1,10 @@
-provider "aws" {
-  region     = "us-east-1"      # Set your desired AWS region
-  aws_access_key   = "AKIAVFIWISPQLW3I7KCQ"
-  aws_secret_key   = "/ny2qqFiNaMObSAB7Xu1lauP5muQBS5FJ46kNeH6"
-}
+terraform {
+   required_version = ">= 1.0.0"
+ 
+   required_providers {
+     aws = {
+       source  = "hashicorp/aws"
+       version = "~> 5.0"
+     }
+   }
+ }
