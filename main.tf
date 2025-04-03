92597601bf5efa61ce95180bc162cf8952e25d42
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-03 23:33:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ provider "aws" {
 
 module "VM01" {
   source        =  "./modules/test"  # Path to the module directory
-  #instance_type =  "var.aws_instance_type" 
-  #ami_id        =  "ami-08b5b3a93ed654d19"  # Replace with yourI ID
+  instance_type =  "t2.micro" 
+  ami_id        =  "ami-08b5b3a93ed654d19"  # Replace with yourI ID
   #aws_access_key       = var.aws_access_key
   #aws_secret_key       = var.aws_secret_key
   #instance_name = "my-terraform-vm"
