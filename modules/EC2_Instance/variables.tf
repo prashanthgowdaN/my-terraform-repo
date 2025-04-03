857b88734e396ef9f80901546ec8ee393974a401
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-03 16:32:25 +0530

    Update variables.tf

--- modules/EC2_Instance/variables.tf
+++ modules/EC2_Instance/variables.tf
@@ -7,3 +7,15 @@ variable "instance_type" {
   description = "The type of instance to create"
   type        = string
 }
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
