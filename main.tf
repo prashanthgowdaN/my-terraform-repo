92b0ddb5c20b0ae702e0808ca2a0e4b6c3b69f66
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-03 18:15:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,16 @@ provider "aws" {
    secret_key = var.aws_secret_key
 }
 
-resource "aws_instance" "Template" {
+module "EC2_VM01" {
+  source        =  "./modules/EC2_Instance"  # Path to the module directory
+  instance_type =  "var.aws_instance_type" 
+  ami_id        =  "ami-08b5b3a93ed654d19"  # Replace with yourI ID
+  #access_key       = var.aws_access_key
+  #secret_key       = var.aws_secret_key
+  #instance_name = "my-terraform-vm"
+}
+
+/*resource "aws_instance" "Template" {
   ami = "ami-08b5b3a93ed654d19" 
   instance_type  = "t2.micro"
   #key_name      = var.key_name
@@ -22,20 +31,13 @@ resource "aws_instance" "Template" {
 
 # main.tf (root directory)
 
-/*module "EC2_VM02" {
+module "EC2_VM02" {
   source        =  "./modules/dev"  # Path to the module directory
   instance_type =  "var.aws_instance_type" 
   ami_id        =  "ami-08b5b3a93ed654d19"  # Replace with yourI ID
   aws_access_key       = var.aws_access_key
   aws_secret_key       = var.aws_secret_key
   #instance_name = "my-terraform-vm"
-}
-
-module "EC2_VM01" {
-  source        =  "./modules/EC2_Instance"  # Path to the module directory
-  instance_type =  "var.aws_instance_type" 
-  ami_id        =  "ami-08b5b3a93ed654d19"  # Replace with yourI ID
-  #access_key       = var.aws_access_key
-  #secret_key       = var.aws_secret_key
-  #instance_name = "my-terraform-vm"
 }*/
+
+
