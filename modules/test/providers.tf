7eb573bd68c39ea7ba33a5b98f7d33efd6cad5da
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-03 19:54:42 +0530

    Update providers.tf

--- modules/test/providers.tf
+++ modules/test/providers.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region     = "us-east-1"      # Set your desired AWS region
+  access_key = var.aws_access_key  # Reference to a variable for access key
+  secret_key = var.aws_secret_key  # Reference to a variable for secret key
+}
+
 terraform {
    required_version = ">= 1.0.0"
  
