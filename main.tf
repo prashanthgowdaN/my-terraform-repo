54f19cb6c6e22930769d058d84fdb0d286ec76bc
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-03 18:12:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-/*terraform {
+terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -11,16 +11,6 @@ provider "aws" {
    access_key = var.aws_access_key
    secret_key = var.aws_secret_key
 }
-# main.tf (root directory)
-
-module "EC2_VM02" {
-  source        =  "./modules/dev"  # Path to the module directory
-  instance_type =  "var.aws_instance_type" 
-  ami_id        =  "ami-08b5b3a93ed654d19"  # Replace with yourI ID
-  aws_access_key       = var.aws_access_key
-  aws_secret_key       = var.aws_secret_key
-  #instance_name = "my-terraform-vm"
-}
 
 resource "aws_instance" "Template" {
   ami = "ami-08b5b3a93ed654d19" 
@@ -28,6 +18,17 @@ resource "aws_instance" "Template" {
   #key_name      = var.key_name
   #subnet_id     = data.aws_subnet_ids.default_subnet.ids[0]  # Use the default subnet ID
   #vpc_security_group_ids = [data.aws_security_group.default_sg.id]  # Use the default security group
+}
+
+# main.tf (root directory)
+
+/*module "EC2_VM02" {
+  source        =  "./modules/dev"  # Path to the module directory
+  instance_type =  "var.aws_instance_type" 
+  ami_id        =  "ami-08b5b3a93ed654d19"  # Replace with yourI ID
+  aws_access_key       = var.aws_access_key
+  aws_secret_key       = var.aws_secret_key
+  #instance_name = "my-terraform-vm"
 }
 
 module "EC2_VM01" {
