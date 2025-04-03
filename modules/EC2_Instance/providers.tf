ca96431ad63aac31dee8803942a80d0bc6b9e352
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-03 18:45:13 +0530

    Update providers.tf

--- modules/EC2_Instance/providers.tf
+++ modules/EC2_Instance/providers.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAVFIWISPQLW3I7KCQ"
+  secret_key = "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"
+}
+
 terraform {
    required_version = ">= 1.0.0"
  
