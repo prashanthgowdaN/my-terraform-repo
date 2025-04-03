61025b6ad8638cee1734b9a2cfc48484dfa330d7
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-03 19:57:22 +0530

    Update providers.tf

--- modules/test/providers.tf
+++ modules/test/providers.tf
@@ -3,14 +3,3 @@ provider "aws" {
   access_key = var.aws_access_key  # Reference to a variable for access key
   secret_key = var.aws_secret_key  # Reference to a variable for secret key
 }
-
-terraform {
-   required_version = ">= 1.0.0"
- 
-   required_providers {
-     aws = {
-       source  = "hashicorp/aws"
-       version = "~> 5.0"
-     }
-   }
- }
