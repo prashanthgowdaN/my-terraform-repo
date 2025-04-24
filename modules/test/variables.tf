202805b266d7f3e4612ac47af4333e6847f16964
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-24 19:17:23 +0530

    Update variables.tf

--- modules/test/variables.tf
+++ modules/test/variables.tf
@@ -1,4 +1,4 @@
-variable "aws_access_key" {
+/*variable "aws_access_key" {
   description = "AWS access key"
   type        = string
   sensitive   = true
@@ -10,7 +10,7 @@ variable "aws_secret_key" {
   type        = string
   sensitive   = true
   default     = "C+4Sz7EKMOZBKktZmBh6qoKHtkAvmWC4f5NCZ7bn"
-}
+}*/
 
 variable "ami_id" {
   description = "AMI ID for EC2 in Dev environment"
