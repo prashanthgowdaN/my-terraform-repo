2b5f45931cd9c6ec2e49456ca0cec98e426528b7
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-25 13:57:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"      # Set your desired AWS region
-  #access_key   = "AKIAVFIWISPQCN2UACWS"
-  #secret_key   = "KD1Udy11ApMV143mk6VedWxSBWdKxvQqiZiym5JE"
+  aws_access_key   = "AKIAVFIWISPQCN2UACWS"
+  aws_secret_key   = "KD1Udy11ApMV143mk6VedWxSBWdKxvQqiZiym5JE"
 }
 
 module "VM01" {
