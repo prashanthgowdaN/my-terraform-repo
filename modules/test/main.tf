4d7937ceb5bab3e1b5fa130ecd0ee932536f547d
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-25 13:19:55 +0530

    Update main.tf

--- modules/test/main.tf
+++ modules/test/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"      # Set your desired AWS region
-  access_key   = "AKIAVFIWISPQJEXFY4UN"
-  secret_key   = "QlmYZY4kSd7D6PpOzHDqMOMDqIeqQ3tCwbZcUkQ4"
+  access_key   = var.aws_access_key
+  secret_key   = var.aws_secret_key
 }
 
 
