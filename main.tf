8ea42f9d79550d104ef65f18d6daef09b692dd74
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-04-25 14:03:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 provider "aws" {
   region     = "us-east-1"      # Set your desired AWS region
+  access_key   = "AKIAVFIWISPQCN2UACWS"
+  secret_key   = "KD1Udy11ApMV143mk6VedWxSBWdKxvQqiZiym5JE"
 }
 
 module "VM01" {
   source        =  "./modules/test"  # Path to the module directory
   instance_type =  "t2.micro" 
   ami_id        =  "ami-08b5b3a93ed654d19"  # Replace with yourI ID
-  aws_access_key       = var.aws_access_key
-  aws_secret_key       = var.aws_secret_key
+  #aws_access_key       = var.aws_access_key
+  #aws_secret_key       = var.aws_secret_key
   #instance_name = "my-terraform-vm"
 }
 
